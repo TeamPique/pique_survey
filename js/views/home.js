ee8749a32c984d62370e9f4a2041e4475d57695c
Evan Turner <evturn@gmail.com>
2015-04-01 16:24:08 -0400

    building font page to swap survey and form

---
+++ js/views/home.js
@@ -0,0 +1,10 @@
+var Home = Backbone.View.extend({
+	el: '#home',
+	surveyHeaderTemplate: _.template($('#survey-header-template').html()),
+	initialize: function() {
+
+	},
+	render: function() {
+
+	},
+});

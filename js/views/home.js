95aa93b4c0449c14b4cbc16452fcd36d3219600d
Evan Turner <evturn@gmail.com>
2015-04-01 16:27:31 -0400

    creating form for register

--- js/views/home.js
+++ js/views/home.js
@@ -1,10 +1,16 @@
 var Home = Backbone.View.extend({
-	el: '#home',
+	el: '#header',
 	surveyHeaderTemplate: _.template($('#survey-header-template').html()),
+	registerTemplate: _.template($('#register-template').html()),
 	initialize: function() {
-
+		this.register();
 	},
-	render: function() {
-
+	survey: function() {
+		this.$el.html(this.surveyTemplate());
+		return this;
+	},
+	register: function() {
+		this.$el.html(this.registerTemplate());
+		return this;
 	},
 });
--- js/views/nav-view.js
+++ js/views/nav-view.js
@@ -2,6 +2,7 @@ var NavView = Backbone.View.extend({
 	el: '#nav',
 	navTemplate: _.template($('#nav-template').html()),
 	initialize: function() {
+		var home = new Home();
 		this.render();
 	},
 	render: function() {

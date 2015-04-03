a6b565c0dabfe1bf95eb5a4d1db407ee5db09f65
Evan Turner <evturn@gmail.com>
2015-04-02 23:32:21 -0400

    clock created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,39 @@
 var beta = beta || {};
-var collection = new beta.Users();
 
+var collection = new beta.Users();
 new beta.App({collection: collection});
 
 new WOW(
     { offset: 120 }
 ).init();
 
-$(function() {
-	moment().format();
 
-	 $('#clock').livestamp(new Date('April 22, 2015'));
-});
+var end = new Date('04/22/2015 12:0 AM');
 
+  var _second = 1000;
+  var _minute = _second * 60;
+  var _hour = _minute * 60;
+  var _day = _hour * 24;
+  var timer;
 
+  function showRemaining() {
+      var now = new Date();
+      var distance = end - now;
+      if (distance < 0) {
+
+          clearInterval(timer);
+          document.getElementById('countdown').innerHTML = 'Pique is now live';
+          return;
+      }
+      var days = Math.floor(distance / _day);
+      var hours = Math.floor((distance % _day) / _hour);
+      var minutes = Math.floor((distance % _hour) / _minute);
+      var seconds = Math.floor((distance % _minute) / _second);
+
+      document.getElementById('countdown').innerHTML = days + 'days ';
+      document.getElementById('countdown').innerHTML += hours + 'hrs ';
+      document.getElementById('countdown').innerHTML += minutes + 'mins ';
+      document.getElementById('countdown').innerHTML += seconds + 'secs';
+  }
+
+  timer = setInterval(showRemaining, 1000);

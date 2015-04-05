49b238f9253ab5842bdc391a05781f5a54d6ab2a
Evan Turner <evturn@gmail.com>
2015-04-05 19:17:38 -0400

    hidden div now showing on click

--- assets/js/views/team.app.js
+++ assets/js/views/team.app.js
@@ -2,13 +2,22 @@ var team = team || {};
 
 team.App = Backbone.View.extend({
 	el: '#team',
+	initialize: function() {
+		$('.team-desc').hide();
+	},
 	events: {
-		'click .team-member' : 'popup'
+		'click .photo' : 'popup'
 	},
 	popup: function(e) {
-		console.log('Sup?');
+		e.preventDefault();
+		var $description = $(e.currentTarget);
+		$description.find('.team-desc').slideDown('slow').mouseleave(function() {
+			$('.team-desc').slideUp('slow');
+		});
+    
+
+    
 	},
 });
 
 new team.App();
-

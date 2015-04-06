9b74b7d5ca85e19180d5b3bdb3cbc982c9873370
Evan Turner <evturn@gmail.com>
2015-04-06 03:27:05 -0400

    hour on clock fixed

--- assets/js/views/beta.app.js
+++ assets/js/views/beta.app.js
@@ -8,7 +8,7 @@ beta.App = Backbone.View.extend({
     new team.App({collection: collection});
     $('#team').hide();
 		this.form();
-		this.countdown('04/22/2015 12:0 AM', 'countdown');
+		this.countdown('04/22/2015 12:0 AM');
 	},
 	events: {
 		'click #register-btn'     : 'create',
@@ -48,24 +48,26 @@ beta.App = Backbone.View.extend({
     this.collection.create({name: name, email: email});
     this.modal();
   },
-	countdown: function(date, id) {
+	countdown: function(date) {
     var launch = new Date(date);
-    var sec = 1000;
-    var min = sec * 60;
-    var hour = min * 60;
+    var second = 1000;
+    var minute = second * 60;
+    var hour = minute * 60;
     var day = hour * 24;
+    var time;
+
     function count() {
       var now = new Date();
       var timespan = launch - now;
       if (timespan < 0) {
         clearInterval(timer);
-        document.getElementById(id).innerHTML = 'Pique is now live';
+        document.getElementById('countdown').innerHTML = 'Pique is now live';
         return false;
       }
       var days = Math.floor(timespan / day);
-      var hours = Math.floor((timespan % days) / hour);
-      var minutes = Math.floor((timespan % hour) / min);
-      var seconds = Math.floor((timespan % min) / sec);
+      var hours = Math.floor((timespan % day) / hour);
+      var minutes = Math.floor((timespan % hour) / minute);
+      var seconds = Math.floor((timespan % minute) / second);
       $('#days').html('<h2>' + days + '</h2><p>days</p>');
       $('#hours').html('<h2>' + hours + '</h2><p>hours</p>');
       $('#minutes').html('<h2>' + minutes + '</h2><p>minutes</p>');

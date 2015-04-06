17161f67fb54d1196a04fe1c7f2e65c5d7b0f5a6
Evan Turner <evturn@gmail.com>
2015-04-06 00:04:37 -0400

    fadeIn and fadeOut added on team-images

--- assets/js/views/team.app.js
+++ assets/js/views/team.app.js
@@ -11,7 +11,8 @@ team.App = Backbone.View.extend({
 	events: {
 		'mouseover .photo' 		 		 : 'popup',
 		'click #team-register-btn' : 'beta',
-		'click #team-logo'				 : 'beta'
+		'click #team-logo'				 : 'beta',
+		'click #team-register-btn' : 'create'
 	},
 	beta: function(e) {
 		e.preventDefault();
@@ -24,11 +25,19 @@ team.App = Backbone.View.extend({
 		$('#team-register-form').append('<p class="title footer">Proudly made in NYC</p>');
 		return this;
 	},
+	create: function(e) {
+		e.preventDefault();
+		var name = $('#name').val().replace(/[^@.][[:punct:]]/g,"");
+		var email = $('#email').val().replace(/[^@.][[:punct:]]/g,"");
+		console.log(email);
+		this.collection.create({name: name, email: email});
+    this.thanks();
+	},
 	popup: function(e) {
 		e.preventDefault();
 		var $description = $(e.currentTarget);
-		$description.find('.team-desc').slideDown('slow').mouseleave(function() {
-			$('.team-desc').slideUp('slow');
+		$description.find('.team-desc').fadeIn('slow').mouseleave(function() {
+			$('.team-desc').fadeOut('slow');
 		}); 
 	},
 });

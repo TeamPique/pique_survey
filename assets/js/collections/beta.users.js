c3da5b7cf616a1a519491aaeefe67100a6efb7eb
Sean Jackson <sjackson44@gmail.com>
2015-04-07 12:01:59 -0400

    Moved to main

--- assets/js/collections/beta.users.js
+++ assets/js/collections/beta.users.js
@@ -1,8 +1,5 @@
 var beta = beta || {};
 
-Backbone.sync = function(method, collection, options) {
-        console.log(method, collection options)
-}
 
 beta.Users = Backbone.Collection.extend({
 	model: beta.User,
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@ var beta = beta || {};
 
 var ENTER_KEY = 13
 
+Backbone.sync = function(method, collection, options) {
+        console.log(method, collection options)
+}
+
 var collection = new beta.Users();
 new beta.App({collection: collection});
 

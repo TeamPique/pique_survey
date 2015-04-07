d50ecef80b8ad043ac05f9b56a5670fb390ef7e6
Sean Jackson <sjackson44@gmail.com>
2015-04-07 12:33:41 -0400

    root

--- assets/js/collections/beta.users.js
+++ assets/js/collections/beta.users.js
@@ -3,7 +3,7 @@ var beta = beta || {};
 
 beta.Users = Backbone.Collection.extend({
 	model: beta.User,
-	rootUrl: 'http://pique-beta-serv.herokuapp.com/user',
+	root: 'http://pique-beta-serv.herokuapp.com/user',
 });
 
 

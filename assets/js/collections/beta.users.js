0a91aad68742c703ff65e002f1932a2731da49fd
Sean Jackson <sjackson44@gmail.com>
2015-04-07 11:55:59 -0400

    sync override

--- assets/js/collections/beta.users.js
+++ assets/js/collections/beta.users.js
@@ -1,8 +1,29 @@
 var beta = beta || {};
 
+(function() {
+
+  var proxiedSync = Backbone.sync;
+
+  Backbone.sync = function(method, model, options) {
+    options || (options = {});
+
+    if (!options.crossDomain) {
+      options.crossDomain = true;
+    }
+
+    if (!options.xhrFields) {
+      options.xhrFields = {withCredentials:true};
+    }
+
+    return proxiedSync(method, model, options);
+  };
+})();
+
 beta.Users = Backbone.Collection.extend({
 	model: beta.User,
 	url: 'http://pique-beta-serv.herokuapp.com/user',
 });
+
+
 
 

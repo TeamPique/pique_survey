2c9069a5e316cdad59ffa02970d599696b00164d
Sean Jackson <sjackson44@gmail.com>
2015-04-07 13:14:27 -0400

    so tired

--- assets/js/collections/beta.users.js
+++ assets/js/collections/beta.users.js
@@ -3,7 +3,20 @@ var beta = beta || {};
 
 beta.Users = Backbone.Collection.extend({
 	model: beta.User,
-	root: 'http://pique-beta-serv.herokuapp.com/user',
+	initialize: function () {
+      var that = this;
+      // Hook into jquery
+      // Use withCredentials to send the server cookies
+      // The server must allow this through response headers
+      $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
+        // options.xhrFields = {
+        //   withCredentials: true
+        // };
+          jqXHR.setRequestHeader('X-CSRF-Token', that.get('_csrf'));
+        }
+      });
+    },
+	url: 'http://pique-beta-serv.herokuapp.com/user'
 });
 
 

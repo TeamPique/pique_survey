c510a5c4cc818ada84f3caa12cdcee63c167fe55
Sean Jackson <sjackson44@gmail.com>
2015-04-07 13:23:29 -0400

    start

--- assets/js/collections/beta.users.js
+++ assets/js/collections/beta.users.js
@@ -3,18 +3,18 @@ var beta = beta || {};
 
 beta.Users = Backbone.Collection.extend({
 	model: beta.User,
-	initialize: function () {
-      var that = this;
-      // Hook into jquery
-      // Use withCredentials to send the server cookies
-      // The server must allow this through response headers
-      $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
-        // options.xhrFields = {
-        //   withCredentials: true
-        // };
-          jqXHR.setRequestHeader('Access-Control-Allow-Origin', 'http://piqueapp.github.io');
-      });
-    },
+	// initialize: function () {
+ //      var that = this;
+ //      // Hook into jquery
+ //      // Use withCredentials to send the server cookies
+ //      // The server must allow this through response headers
+ //      $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
+ //        // options.xhrFields = {
+ //        //   withCredentials: true
+ //        // };
+ //          jqXHR.setRequestHeader('Access-Control-Allow-Origin', 'http://piqueapp.github.io');
+ //      });
+ //    },
 	url: 'http://pique-beta-serv.herokuapp.com/user'
 });
 

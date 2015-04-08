bddb17f70143953f5f8bcbcf0e8b12f56ea78ff7
Evan Turner <evturn@gmail.com>
2015-04-08 02:26:46 -0400

    team page also given validation functions

--- assets/js/views/team.app.js
+++ assets/js/views/team.app.js
@@ -13,7 +13,7 @@ team.App = Backbone.View.extend({
 		'mouseover .photo' 		 		: 'popup',
 		'click #team-beta-btn' 		: 'beta',
 		'click #team-nav-img'	 		: 'beta',
-		'click #register-btn'  		: 'create',
+		'click #register-btn'  		: 'createOnClick',
 		'keypress .register-form' : 'createOnEnter'
 	},
 	beta: function(e) {
@@ -32,25 +32,47 @@ team.App = Backbone.View.extend({
     $('#team-thanks').html(this.modalTpl());
     return this;
   },
-	create: function(e) {
+  send: function() {
+    ref.on("value", function(snapshot) {
+      console.log('Firebase Collection', snapshot.val());
+      }.bind(this), function (errorObject) {
+          console.log("The read failed: " + errorObject.code);
+    });
+  },
+  validate: function(email) {
+    var regEx = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
+    return regEx.test(email);
+  },
+  create: function() {
+    var name = $('#name').val();
+    var email = $('#email').val();
+
+    if (email !== "") {
+      if (!this.validate(email)) {
+        $('#email-error').text('Please enter a valid email address');
+        $('#email-error').css({color: 'red'});
+        $('#email').focus();
+        return false;  
+      }
+    } 
+
+    this.collection.create({
+        name: name,
+        email: email
+      });
+
+    this.modal()
+    
+  },
+	createOnClick: function(e) {
 		e.preventDefault();
-		var name = $('#name').val().replace(/[^a-zA-Z0-9 @.\-\_\^]/g,"");
-		var email = $('#email').val().replace(/[^a-zA-Z0-9 @.\-\_\^]/g,"").toLowerCase().replace(" ","");
-    if (name != "" && email != ""){
-      this.collection.create({name: name, email: email});
-    }
-    this.modal();
+    this.create();
 	},
   createOnEnter: function(e) {
     if (e.which !== ENTER_KEY || !this.$input.val().trim()) {
       return;
     }
-    var name = $('#name').val().replace(/[^a-zA-Z0-9 @.\-\_\^]/g,"");
-    var email = $('#email').val().replace(/[^a-zA-Z0-9 @.\-\_\^]/g,"").toLowerCase().replace(" ","");
-    if (name != "" && email != ""){
-      this.collection.create({name: name, email: email});
-    }
-    this.modal();
+    this.create();
   },
 	popup: function(e) {
 		e.preventDefault();

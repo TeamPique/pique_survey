3e4aaaec8e8782d0fd7fde96993aaa40bc6be8a9
Evan Turner <evturn@gmail.com>
2015-04-09 17:58:20 -0400

    media queries moving font sizes

--- assets/js/routers/router.js
+++ assets/js/routers/router.js
@@ -15,7 +15,7 @@ beta.Router = Backbone.Router.extend({
 	},
 	team: function() {
 		$('#beta').hide();
-    $('body').css('background', '#fff');
+    $('body').css('background', '#70BDFC');
     $('#register-form').empty();
     new team.App({collection: collection});
     $('#team').show();

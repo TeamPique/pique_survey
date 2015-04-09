62c9f1ef000dcd5f1bd1d4f98a945b983810ba23
Evan Turner <evturn@gmail.com>
2015-04-09 13:25:18 -0400

    jquery fullPage added for images

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,12 @@ var beta = beta || {};
 var ENTER_KEY = 13
 var FIREBASE_URL  = new Firebase('https://pique.firebaseio.com');
 var ref = new Firebase(FIREBASE_URL + '/users');
+
 new WOW(
     { offset: 120 }
 ).init();
+
+$('#fullpage').fullpage();
 
 var collection = new beta.Users();
 var router = new beta.Router();
